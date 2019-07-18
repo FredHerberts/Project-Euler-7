8aad82bb4910b37847f5509bc199f3fef9dd5ab7
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-18 15:26:10 +0200

    Create v2.py

---
+++ v2.py
@@ -0,0 +1,21 @@
+import time
+import math
+start = time.time()
+primelist= [2]
+
+def primefinder(number):
+    prime = [True for x in range(number + 1)]
+    for y in range(3,int(math.sqrt(number+1)),2):
+        if prime[y] == True:
+            primelist.append(y)
+            for x in range(y+y, number + 1,y):
+                prime[x] = False
+    for x in range(y+2,number+1,2):
+        if prime[x] == True:
+            primelist.append(x)
+
+primefinder(130000)
+print(primelist[10000])
+
+end = time.time()
+print(end - start)
