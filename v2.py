3683e6d4f7b5f40f6f39d58345d72e1116b6d23e
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-19 08:50:25 +0200

    Update v2.py

--- v2.py
+++ v2.py
@@ -4,7 +4,7 @@ start = time.time()
 primelist= [2]
 
 def primefinder(number):
-    prime = [True for x in range(number + 1)]
+    prime = [True] * (number+1)
     for y in range(3,int(math.sqrt(number+1)),2):
         if prime[y] == True:
             primelist.append(y)
